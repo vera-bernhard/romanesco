d8798b2e39c081a8c04e6fc531de5264339348da
veral <veralara.bernhard@uzh.ch>
2019-04-30 01:35:07 +0200

    stuff

--- romanesco/const.py
+++ romanesco/const.py
@@ -14,16 +14,16 @@ VOCAB_FILENAME = 'vocab.json'
 CONFIG_FILENAME = 'config.json'
 
 VOCAB_SIZE = 20000 # vocab size is 41607
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 
 NUM_EPOCHS = 10
 
 # num_steps and learning_rate are hardcoded here; at the moment,
 # the only way to change them is to edit this file
-NUM_STEPS = 500 # truncated backprop length
+NUM_STEPS = 50 # truncated backprop length
 LEARNING_RATE = 0.001
 
-HIDDEN_SIZE = 2048  # RNN state size
+HIDDEN_SIZE = 1024  # RNN state size
 EMBEDDING_SIZE = 256  # embedding vector size
 
 SAMPLE_LENGTH = 100 # only neede for sampling
