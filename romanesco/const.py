5974f3316508d527e03d72a75e1d501cab80747f
veral <veralara.bernhard@uzh.ch>
2019-04-29 16:05:34 +0200

    smaller learning rate

--- romanesco/const.py
+++ romanesco/const.py
@@ -16,12 +16,12 @@ CONFIG_FILENAME = 'config.json'
 VOCAB_SIZE = 15000
 BATCH_SIZE = 32
 
-NUM_EPOCHS = 10
+NUM_EPOCHS = 5
 
 # num_steps and learning_rate are hardcoded here; at the moment,
 # the only way to change them is to edit this file
 NUM_STEPS = 100 # truncated backprop length
-LEARNING_RATE = 0.005
+LEARNING_RATE = 0.001
 
 HIDDEN_SIZE = 1024  # RNN state size
 EMBEDDING_SIZE = 256  # embedding vector size
