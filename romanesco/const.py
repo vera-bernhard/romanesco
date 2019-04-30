2665adc84f3b2edecc6a14f049525a75197f08ac
veral <veralara.bernhard@uzh.ch>
2019-04-30 02:34:33 +0200

    only modification: has GRU

--- romanesco/const.py
+++ romanesco/const.py
@@ -13,7 +13,7 @@ VOCAB_FILENAME = 'vocab.json'
 
 CONFIG_FILENAME = 'config.json'
 
-VOCAB_SIZE = 20000 # vocab size is 41607
+VOCAB_SIZE = 10000 # vocab size is 41607
 BATCH_SIZE = 64
 
 NUM_EPOCHS = 10
