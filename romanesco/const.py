83f96513bfcfe5cd9bc6df84c2f9b09226a8f158
veral <veralara.bernhard@uzh.ch>
2019-04-30 02:58:42 +0200

    more iterations

--- romanesco/const.py
+++ romanesco/const.py
@@ -13,10 +13,10 @@ VOCAB_FILENAME = 'vocab.json'
 
 CONFIG_FILENAME = 'config.json'
 
-VOCAB_SIZE = 800 # vocab size is 41607
+VOCAB_SIZE = 20000 # vocab size is 41607
 BATCH_SIZE = 32
 
-NUM_EPOCHS = 10
+NUM_EPOCHS = 13
 
 # num_steps and learning_rate are hardcoded here; at the moment,
 # the only way to change them is to edit this file
